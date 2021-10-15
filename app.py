bbe1430ae3ffcec3b3465264bcbec126db46e106
leo-step <leo.stepanewk@gmail.com>
2021-10-15 15:56:46 -0400

    cas login for all, event list and reponse pages with id as url param

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
+from apiclient.discovery import build
 from cas import CASClient
 import datetime
 from flask import Flask, redirect, render_template, request, session, url_for
-from apiclient.discovery import build
-from oauth2client import client
 import json
+from oauth2client import client
 import os
 
 app = Flask(__name__)
@@ -17,71 +17,24 @@ cas_client = CASClient(
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if "netID" not in session:
+        return redirect(url_for("login"))
+    return "render template for events list here"
 
-@app.route("/events")
-def events():
-    if "username" not in session:
+@app.route("/event")
+def event():
+    session["eventID"] = request.args.get("id")
+    if "netID" not in session:
+        return redirect(url_for("login"))
+    eventID = session.pop("eventID", None)
+    if not eventID:
         return redirect(url_for("index"))
-    
-    event_summaries = []
-    if "credentials" in session:
-
-        creds = client.OAuth2Credentials.from_json(session["credentials"])
-        service = build('calendar', 'v3', credentials=creds)
-        now = datetime.datetime.utcnow().isoformat() + 'Z'
-        events_result = service.events().list(calendarId="primary",
-                                        timeMin=now, maxResults=10,
-                                        singleEvents=True, orderBy="startTime"
-                                        ).execute()
-        events = events_result.get("items", [])
-
-        if events:
-            for event in events:
-                event_summaries.append(event["summary"])
-
-    print(event_summaries)
-
-    return render_template("events.html", username=session["username"], 
-                                    displayname=session["displayname"],
-                                    events=event_summaries)
-
-@app.route("/importgcal")
-def importgcal():
-    if "username" not in session:
-        return redirect(url_for("index"))
-
-    if "credentials" not in session:
-        return redirect(url_for("oauth2callback"))
-
-    credentials = client.OAuth2Credentials.from_json(session["credentials"])
-    
-    if credentials.access_token_expired:
-        return redirect(url_for("oauth2callback"))
-
-    return redirect(url_for("events"))
-
-@app.route("/oauth2callback")
-def oauth2callback():
-    flow = client.flow_from_clientsecrets( # store client secret as config var
-        "client_secret.json",              # create json file on program start
-        scope = "https://www.googleapis.com/auth/calendar",
-        redirect_uri = url_for("oauth2callback", _external=True)
-    )
-
-    if "code" not in request.args:
-        auth_uri = flow.step1_get_authorize_url()
-        return redirect(auth_uri)
-
-    auth_code = request.args.get("code")
-    credentials = flow.step2_exchange(auth_code)
-    session["credentials"] = credentials.to_json()
-    return redirect(url_for("importgcal"))
+    return "eventID: {}".format(eventID)
 
 @app.route("/login")
 def login():
-    if "username" in session:
-        return redirect(url_for("events"))
+    if "netID" in session:
+        return redirect(url_for("index"))
 
     next = request.args.get("next")
     ticket = request.args.get("ticket")
@@ -95,8 +48,10 @@ def login():
     if not user:
         return "Failed to verify ticket."
 
-    session["username"] = user
-    session["displayname"] = attributes["displayname"]
+    session["netID"] = user
+    
+    if "eventID" in session:
+        return redirect(url_for("event", id=session["eventID"]))
 
     return redirect(next)
 
@@ -108,11 +63,8 @@ def logout():
 
 @app.route("/logout_callback")
 def logout_callback():
-    session.pop("username", None)
+    session.pop("netID", None)
     return redirect(url_for("index"))
 
-
 if __name__ == "__main__":
-    with open("client_secret.json", "w") as f:
-        json.dump(json.loads(os.environ.get("GOOGLE_CLIENT_SECRET")), f)
     app.run()
