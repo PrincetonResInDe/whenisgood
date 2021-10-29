d33c73a04a6a4af73758b14d1f2a61bfbfac2e65
leo-step <leo.stepanewk@gmail.com>
2021-10-28 21:13:32 -0400

    api error handling

--- api.py
+++ api.py
@@ -2,22 +2,25 @@ import mysql.connector
 import os
 
 def call_proc(name, args=()):
-    db = mysql.connector.connect(
-        host=os.environ.get("DB_HOST"),
-        database=os.environ.get("DB_NAME"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD")
-    )
-    cursor = db.cursor()
-    cursor.callproc(name, args)
-    db.commit()
-    result = None
-    for stored_result in cursor.stored_results():
-        result = stored_result
-        break
-    output = []
-    if result:
-        columns = [data[0] for data in result.description]
-        for row in result.fetchall():
-            output.append(dict(zip(columns, row)))
-    return output
+    try:
+        db = mysql.connector.connect(
+            host=os.environ.get("DB_HOST"),
+            database=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD")
+        )
+        cursor = db.cursor()
+        cursor.callproc(name, args)
+        db.commit()
+        result = None
+        for stored_result in cursor.stored_results():
+            result = stored_result
+            break
+        output = []
+        if result:
+            columns = [data[0] for data in result.description]
+            for row in result.fetchall():
+                output.append(dict(zip(columns, row)))
+        return output
+    except:
+        return [{"result": "failure"}]
